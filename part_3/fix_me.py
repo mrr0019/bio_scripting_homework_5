12889f0eed9209ca1be7a767f2948ab250a2f519
Megan Roberts <mrr0019@auburn.edu>
2020-02-26 15:52:09 -0600

    Fixed integer of a and b

--- part_3/fix_me.py
+++ part_3/fix_me.py
@@ -1,11 +1,11 @@
 #! /bin/bash/python3
 
 # Reading data from the user's input
-a = input("Enter a : ")
+int(a = input("Enter a : "))
 
 print("You entered", a, "which is a", type(a))
 
-b = input("Enter b : ")
+int(b = input("Enter b : "))
 
 print("You entered", b, "which is a", type(b))
 
