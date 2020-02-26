f822b22e987bed6ab08e1e6891aababe4bcec1e1
Megan Roberts <mrr0019@auburn.edu>
2020-02-26 14:20:29 -0600

    Removed appending b/c looped- will do manually in terminal

--- part_2/sys_info_Roberts.sh
+++ part_2/sys_info_Roberts.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Current Date and Time
-echo "Current date and time: $(date)"
+echo "Current date and time: $(date)" 
+
 
 # Currently logged on users
-echo "Currently logged on users: "
+echo "Currently logged on users: " 
 who
 
+
 # System Uptime
-echo "System uptime: "
+echo "System uptime: " 
 uptime
 
-# Appending output to a log file
-./sys_info_Roberts.sh >> sys_info_Roberts_log.txt
+
