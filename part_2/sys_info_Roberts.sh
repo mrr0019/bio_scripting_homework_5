e4952371cf92e41a4efce377fbbaf7e635791fd7
Megan Roberts <mrr0019@auburn.edu>
2020-02-26 13:54:43 -0600

    Added script sys_info_Roberts.sh

---
+++ part_2/sys_info_Roberts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Current Date and Time
+echo "Current date and time: $(date)"
+
+# Currently logged on users
+echo "Currently logged on users: "
+who
+
+# System Uptime
+echo "System uptime: "
+uptime
